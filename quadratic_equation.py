45e4b0f651d6ce8392f6b4fb2438256ca3f63a4a
PavelShturm85 <pavel.shturm@gmail.com>
2017-12-04 23:43:52 +0400

    Скрипт решающий квадратные уравнения и тест для проверки.

---
+++ quadratic_equation.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+
+def get_roots(a, b, c):
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant < 0:
+        return None, None
+    else:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
+        return root1, root2 if discriminant > 0 else None
